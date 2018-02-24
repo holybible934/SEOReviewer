b3004be18207806bf280dd1fb4d630760f540a37
CChang <holybible934@gmail.com>
2018-02-24 20:24:59 +0800

    Revise test.html and main

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-var http = require("http");
+var SeoRules = {
+  ImgWithoutAlt: function() {
+    var fileStream = require('fs'),
+        filename = 'test.html';
+    fileStream.readFile(filename, 'utf8', function(err, data) {
+      if (err) throw err;
+      console.log('OK' + filename);
+      console.log(data);
+    });
+    console.log('ImgWithoutAlt - CHeck image without alt attribute');
+  }
+}
 
-http.createServer(function (request, response) {
-
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+SeoRules.ImgWithoutAlt();
