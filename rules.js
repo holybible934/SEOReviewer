0ed7331e744451eab2a61164067b2ac8582ce1ea
CChang <holybible934@gmail.com>
2018-02-24 19:19:00 +0800

    Add npm package and rule#1 and rule#2 into rules.js

---
+++ rules.js
@@ -0,0 +1,9 @@
+var SeoRules = {
+  function ImgWithoutAlt() {
+    console.log('ImgWithoutAlt - CHeck image without alt attribute');
+  }
+
+  function HrefWithoutRel() {
+    console.log('HrefWithoutRel - CHeck a href without rel attribute');
+  }
+}
