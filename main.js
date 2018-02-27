80baf25de7e172fa02b0b0184aac2bf5b0a0abdc
CChang <holybible934@gmail.com>
2018-02-27 21:20:46 +0800

    1. Implement InputStream from argv[2] 2. Adjust CLI flow

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var StrongTagLimit = 15;
+var inputStreamName = process.argv[2]; // Config Input Stream from the 2nd argument when be executed
+var buf = '';
+var rules = [];
+var ruleCount = 5;
+for (i = 0;i < ruleCount; i++) {
+  rules.push(false);
+}
 
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -8,46 +15,56 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-console.log('Type rule1<ENTER> to execute rule1')
+
+console.log('Type ruleX<ENTER> to execute rule X, where X is 1, 2, 3, 4 or 5.\nThen type any to start scanning.')
 rl.prompt();
 
 rl.on('line', (line) => {
   switch (line.trim()) {
     case 'rule1':
-      ImgWithoutAlt();
+      rules[0] = true;
       break;
     case 'rule2':
-      HrefWithoutRel();
+      rules[1] = true;
+      break;
+    case 'rule3':
+      rules[2] = true;
+      break;
+    case 'rule4':
+      rules[3] = true;
+      break;
+    case 'rule5':
+      rules[4] = true;
       break;
     default:
+      rl.close();
       break;
   }
-  rl.prompt();
-})
-/*
-rl.question('Do you want to enable rule1? If yes, type y\n', function(choice) {
-  if (choice === 'y') {
-    ImgWithoutAlt();
-  }
+}).on('close', function() {
+  console.log('Input is finished. Start scanning');
+  process.exit(0);
+});
+
+const readFileLine = require('readline');
+const fileStream =  require('fs');
+const filerl = readFileLine.createInterface({
+  input: fileStream.createReadStream(inputStreamName),
+  crlfDelay: Infinity
 });
 
 
-
-rl.question('Do you want to enable rule2? If yes, type y\n', function(choice) {
-  if (choice === 'y') {
-    HrefWithoutRel();
-  }
+filerl.on('line', (line) => {
+  buf += line;
 });
-*/
 
 function ImgWithoutAlt() {
-    var fileStream = require('fs'),
-        filename = 'test.html';
-    fileStream.readFile(filename, 'utf8', function(err, data) {
-      if (err) throw err;
-      // TODO - Detect if any <img /> tag without alt attribute
-    });
-    console.log('ImgWithoutAlt - Check image without alt attribute');
+  var fileStream = require('fs');
+  filename = 'test.html';
+  fileStream.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    // TODO - Detect if any <img /> tag without alt attribute
+  });
+  console.log('ImgWithoutAlt - Check image without alt attribute');
 }
 
 function HrefWithoutRel() {
