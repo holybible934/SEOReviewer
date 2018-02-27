7ae3bde36b24ed5f4ea857efd6b78ba09cd94f66
CChang <holybible934@gmail.com>
2018-02-28 04:47:54 +0800

    Fix CountH1Tag bugs

--- main.js
+++ main.js
@@ -36,18 +36,23 @@ rl.on('line', (line) => {
   switch (line.trim()) {
     case 'rule1':
       rules[0] = true;
+      console.log('rule1 is set');
       break;
     case 'rule2':
       rules[1] = true;
+      console.log('rule2 is set');
       break;
     case 'rule3':
       rules[2] = true;
+      console.log('rule3 is set');
       break;
     case 'rule4':
       rules[3] = true;
+      console.log('rule4 is set');
       break;
     case 'rule5':
       rules[4] = true;
+      console.log('rule5 is set');
       break;
     default:
       rl.close();
@@ -99,6 +104,7 @@ function CountStrongTags(line) {
 }
 
 function CountH1Tag(line) {
-  if (line.toUpperCase().includes('<H1>')) {
+  if (line.toUpperCase().includes('<H1')) {
     h1TagCount++;
+  }
 }
