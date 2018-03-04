ba0da4093c6775ca018675497ee0ac432ff5d3b1
CChang <holybible934@gmail.com>
2018-03-04 21:26:12 +0800

    Add POST handler in http-server

--- http-server.js
+++ http-server.js
@@ -5,8 +5,23 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-  const src = fs.createReadStream('./http-server-page.html');
-  src.pipe(res);
+  if (req.method == 'POST') {
+    console.log("POST");
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+        console.log("Partial body: " + body);
+    });
+    req.on('end', function () {
+        console.log("Body: " + body);
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('post received');
+  }
+  else {
+    const src = fs.createReadStream('./http-server-page.html');
+    src.pipe(res);
+  }
 });
 
 server.listen(port, hostname, () => {
